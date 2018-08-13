5c59370b47933e18acc2de1ee4a76a7cccb4efdb
Sabrina Robinson <sabrinarob@gmail.com>
2018-08-13 16:50:01 -0600

    ATP - Implements endpoint to create a food item

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -7,4 +7,14 @@ class Api::V1::FoodsController < ApplicationController
   def show
     render json: Food.find(params[:id])
   end
+
+  def create
+    render json: Food.create(food_params)
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :calories)
+  end
 end
