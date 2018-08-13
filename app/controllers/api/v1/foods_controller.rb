a2a8b636ba63cf1116ee1e5a603709dc59e8cb07
Sabrina Robinson <sabrinarob@gmail.com>
2018-08-13 16:00:35 -0600

    TEST - implements test for one food item endpoint

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::FoodsController < ApplicationController
 
   def index
-    
+    render json: Food.all
   end
 end
--- spec/requests/api/v1/foods_request_spec.rb
+++ spec/requests/api/v1/foods_request_spec.rb
@@ -7,5 +7,21 @@ describe "Foods API" do
     get '/api/v1/foods'
 
     expect(response).to be_successful
+
+    foods = JSON.parse(response.body)
+    expect(foods.count).to eq(3)
+
+    # expect(response.status).to eq(404)
+  end
+
+  it "can get one food item by its id" do
+    id = create(:food).id
+
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(item["id"]).to eq(id)
   end
 end
