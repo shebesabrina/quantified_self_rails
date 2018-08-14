30637d0b2f0db84321dfcfe813ed793cf432c07c
Sabrina Robinson <sabrinarob@gmail.com>
2018-08-14 15:43:36 -0600

    ATP - Implements sad path tests with status codes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :foods, except: [:new, :edit]
+      resources :meals, only: [:index]
+      namespace :meals do
+        get '/:id/foods', to: 'foods#index'
+      end
     end
   end
 end
--- spec/requests/api/v1/foods_request_spec.rb
+++ spec/requests/api/v1/foods_request_spec.rb
@@ -2,27 +2,29 @@ require 'rails_helper'
 
 describe "Foods API" do
   it "returns all foods in database" do
-    create_list(:food, 3)
-
     get '/api/v1/foods'
 
     expect(response).to be_successful
+    foods = JSON.parse(response.body,  symbolize_names: true)
+    expect(foods.count).to eq(0)
 
-    foods = JSON.parse(response.body)
-    expect(foods.count).to eq(3)
-
-    # expect(response.status).to eq(404)
+    food_list = create_list(:food, 3)
+    foods = JSON.parse(response.body,  symbolize_names: true)
+    expect(food_list.count).to eq(3)
   end
 
   it "can get one food item by its id" do
+    get '/api/v1/foods/8675309'
+    expect(status).to eq(404)
+
     id = create(:food).id
 
     get "/api/v1/foods/#{id}"
 
-    food = JSON.parse(response.body)
+    food = JSON.parse(response.body,  symbolize_names: true)
 
     expect(response).to be_successful
-    expect(food["id"]).to eq(id)
+    expect(food[:id]).to eq(id)
   end
 
   it "can create a new food item" do
@@ -33,11 +35,17 @@ describe "Foods API" do
 
     assert_response :success
     expect(response).to be_successful
+    expect(Food.count).to eq(1)
     expect(food.name).to eq(food_params[:name])
   end
 
   it "can update an existing food item" do
     id = create(:food).id
+
+    # params = {}
+    # put "/api/v1/foods/#{id}", params: params
+    # expect(status).to eq(400)
+
     previous_name = Food.last.name
     food_params = { name: "strawberry" }
 
@@ -50,6 +58,9 @@ describe "Foods API" do
   end
 
   it "can destroy a food item" do
+    delete '/api/v1/foods/8675309'
+    expect(status).to eq(404)
+
     food = create(:food)
 
     expect(Food.count).to eq(1)
