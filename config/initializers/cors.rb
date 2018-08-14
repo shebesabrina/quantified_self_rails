236359d3a5ffd056e916256158fef1c763d0e7d8
Sabrina Robinson <sabrinarob@gmail.com>
2018-08-14 17:50:46 -0600

    Implements other front-end url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://quantified-self-fe.herokuapp.com/'
+    origins 'https://shebesabrina.github.io/quantified-self-fe/'
 
     resource '*',
       headers: :any,
