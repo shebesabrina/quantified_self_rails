ddabaed9ac15918dcc5427b662be09185390d3b9
Sabrina Robinson <sabrinarob@gmail.com>
2018-08-15 13:27:22 -0600

    Debugging cors in cors.rb file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://shebesabrina.github.io/quantified-self-fe/'
+    origins 'https://qs-rails-1.herokuapp.com/'
 
     resource '*',
       headers: :any,
