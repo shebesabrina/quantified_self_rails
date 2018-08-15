9cfd4584dd84ac12ad44bc7140f114e1d66b7671
Sabrina Robinson <sabrinarob@gmail.com>
2018-08-15 11:53:39 -0600

    Adds http vers to cors methods

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module QuantifiedSelfRails
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :patch, :delete, :create]
+        resource '*', headers: :any, methods: [:get, :post, :patch, :delete, :create, :options]
       end
     end
     config.api_only = true
